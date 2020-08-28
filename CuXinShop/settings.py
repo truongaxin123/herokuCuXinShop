8f6d36fa980a6cdee54181335d8d9841e1e33617
truongaxin123 <truongaxin123@gmail.com>
2020-08-28 12:04:02 +0700

    edit setting

--- CuXinShop/settings.py
+++ CuXinShop/settings.py
@@ -86,8 +86,8 @@ WSGI_APPLICATION = 'CuXinShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'cuxinshop',
     }
 }
 
