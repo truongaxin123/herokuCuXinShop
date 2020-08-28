e1b30af80f541eeab8ab26cddba6dd85ec73bd28
truongaxin123 <truongaxin123@gmail.com>
2020-08-28 11:53:00 +0700

    edit setting

--- CuXinShop/settings.py
+++ CuXinShop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'theblog',
     'home',
     'ckeditor',
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CuXinShop.urls'
